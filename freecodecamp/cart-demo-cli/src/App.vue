91752d43d65c884d35828e3dba8aeea6b1c733f9
irisdew <ycleardew@gmail.com>
2022-01-16 22:40:14 +0900

    :zap: move header

--- freecodecamp/cart-demo-cli/src/App.vue
+++ freecodecamp/cart-demo-cli/src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <div id="nav">
+      <header class="top-bar spread">
+        <nav class="top-bar-nav">
+          <router-link to="/" class="top-bar-link">
+            <i class="icofont-spoon-and-fork"></i>
+            <span>Home</span>
+          </router-link>
+          <router-link to="/products" class="top-bar-link">
+            <span>Products</span>
+          </router-link>
+          <router-link to="/past-orders" class="top-bar-link">
+            <span>Past Orders</span>
+          </router-link>
+        </nav>
+        <!-- <a @click="toggleSidebar" class="top-bar-cart-link">
+          <i class="icofont-cart-alt icofont-1x"></i>
+          <span>Cart ({{ totalItems }})</span>
+        </a> -->
+      </header>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
   <router-view/>
 </template>
 
--- freecodecamp/cart-demo-cli/src/router/index.js
+++ freecodecamp/cart-demo-cli/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Products from '../views/Products.vue'
 
 const routes = [
   {
@@ -9,9 +9,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: '/products',
+    name: 'Products',
+    component: Products
   }
 ]
 
