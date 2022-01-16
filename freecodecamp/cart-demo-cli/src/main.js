85ff8ccd964fe7e83930aea2931385615ff260fc
irisdew <ycleardew@gmail.com>
2022-01-16 23:42:29 +0900

    :lipstick: add styles files

--- freecodecamp/cart-demo-cli/src/main.js
+++ freecodecamp/cart-demo-cli/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/styles/style.scss'
+// vue cli 환경에서는 css 파일을 js 파일에 바로 import 할 수 있다
+
 createApp(App).use(router).mount('#app')
 // const app = createApp(App)
 // app.use(router)
