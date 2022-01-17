00e5feb24f97d36c91ce8a298bea5741f1510e2c
irisdew <ycleardew@gmail.com>
2022-01-17 22:02:58 +0900

    :zap: add page contents

--- freecodecamp/cart-demo-cli/src/views/Home.vue
+++ freecodecamp/cart-demo-cli/src/views/Home.vue
@@ -1,11 +1,70 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+        <div
+          v-for="(product, i) in inventory.slice(0, 3)"
+          :key="product.id"
+          class="card"
+        >
+          <div class="card-title">
+            {{ product.name }}
+          </div>
+          <div class="card-body">
+            <i :class="'icofont-10x icofont-' + product.icon"></i>
+            <form>
+              <div class="row">
+                <div class="cell">
+                  <label>Type:</label>
+                </div>
+                <div class="cell">
+                  <em>{{ product.type }}</em>
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Price:</label>
+                </div>
+                <div class="cell">${{ product.price.USD }}</div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Quantity:</label>
+                </div>
+                <div class="cell">
+                  <input type="number" v-model.number="product.quantity" />
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-light" @click="addToCart(product.name, i)">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+import food from '../food.json'
+
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  data() {
+    return {
+      inventory: food
+    }
+  },
+};
 </script>
