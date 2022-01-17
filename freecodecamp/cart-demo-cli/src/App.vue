3254a4d3d05e75db359e79018c10354a6d5992e4
irisdew <ycleardew@gmail.com>
2022-01-17 22:15:33 +0900

    :zap: create Sidebar component

--- freecodecamp/cart-demo-cli/src/App.vue
+++ freecodecamp/cart-demo-cli/src/App.vue
@@ -1,28 +1,63 @@
 <template>
-      <header class="top-bar spread">
-        <nav class="top-bar-nav">
-          <router-link to="/" class="top-bar-link">
-            <i class="icofont-spoon-and-fork"></i>
-            <span>Home</span>
-          </router-link>
-          <router-link to="/products" class="top-bar-link">
-            <span>Products</span>
-          </router-link>
-          <router-link to="/past-orders" class="top-bar-link">
-            <span>Past Orders</span>
-          </router-link>
-        </nav>
-        <!-- <a @click="toggleSidebar" class="top-bar-cart-link">
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <!-- <a @click="toggleSidebar" class="top-bar-cart-link">
           <i class="icofont-cart-alt icofont-1x"></i>
           <span>Cart ({{ totalItems }})</span>
         </a> -->
-      </header>
+  </header>
   <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div> -->
-  <router-view/>
+  <router-view />
+
+  <Sidebar
+    v-if="showSidebar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
 </template>
 
-<style>
-</style>
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+
+export default {
+  components: {
+    Sidebar,
+  },
+  data() {
+    return {
+      showSidebar: true,
+    };
+  },
+  methods: {
+    addToCart(name, index) {
+      if (!this.cart[name]) this.cart[name] = 0;
+      this.cart[name] += this.inventory[index].quantity;
+      this.inventory[index].quantity = 0;
+    },
+    toggleSidebar() {
+      this.showSidebar = !this.showSidebar;
+    },
+    removeItem(name) {
+      delete this.cart[name];
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ freecodecamp/cart-demo-cli/src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <aside class="cart-container">
+  <div class="cart">
+    <h1 class="cart-title spread">
+      <span>
+        Cart
+        <i class="icofont-cart-alt icofont-1x"></i>
+      </span>
+      <button class="cart-close" @click="toggle">&times;</button>
+    </h1>
+
+    <div class="cart-body">
+      <table class="cart-table">
+        <thead>
+          <tr>
+            <th><span class="sr-only">Product Image</span></th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Qty</th>
+            <th>Total</th>
+            <th><span class="sr-only">Actions</span></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(quantity, key, i) in cart" :key="i">
+            <td><i :class="'icofont-3x icofont-'+getIcon(key)"></i></td>
+            <td>{{ key }}</td>
+            <td>\${{ getPrice(key) }}</td>
+            <td class="center">{{ quantity }}</td>
+            <td>\${{ (quantity * getPrice(key)).toFixed(2) }}</td>
+            <td class="center">
+              <button @click="remove(key)" class="btn btn-light cart-remove">
+                &times;
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p class="center" v-if="!Object.keys(cart).length"><em>No items in cart</em></p>
+      <div class="spread">
+        <span><strong>Total: </strong>\${{ calculateTotal() }}</span>
+        <button class="btn btn-light">Checkout</button>
+      </div>
+    </div>
+  </div>
+</aside>
+</template>
