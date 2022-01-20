6a2104ba7a509511b87b60815eadacefbe7d6f8f
Cleardew Yoon <ycleardew@gmail.com>
2022-01-21 00:29:36 +0900

    🍔 meal app(on progress) (#2)
    * add meal app template and functions
    
    * add new meal
    
    * add Meal page and practice vue-router
    
    * update meal detail page
    
    * add v-html

---
+++ recipe-app/src/views/Meal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="recipe">
+    <router-link to="/">&lt; Back</router-link>
+    <h1>{{ meal.title }}</h1>
+    <span class="desc" v-html="cleanText(meal.description)"></span>
+    <hr />
+    <p class="place">{{ meal.place }}</p>
+    <div><span v-for="i in meal.star" :key="i">⭐</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    meal() {
+      return this.$store.state.meals.find(
+        (meal) => meal.slug === this.$route.params.slug
+      );
+    },
+  },
+  methods: {
+    cleanText(text) {
+      return text.replace(/\n/g, "<br />");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
