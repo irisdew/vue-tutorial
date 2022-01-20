4802ab182dd3b1cba6361f9520498b0bf9ed282d
irisdew <ycleardew@gmail.com>
2022-01-21 00:20:56 +0900

    update meal detail page

--- recipe-app/src/views/Meal.vue
+++ recipe-app/src/views/Meal.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="recipe">
     <router-link to="/">&lt; Back</router-link>
-    <h1>{{ $route.params.slug }}</h1>
+    <h1>{{ meal.title }}</h1>
+    <p class="desc">{{ meal.description }}</p>
+    <hr />
+    <p class="place">{{ meal.place }}</p>
+    <div><span v-for="i in meal.star" :key="i">⭐</span></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    meal() {
+      return this.$store.state.meals.find(
+        (meal) => meal.slug === this.$route.params.slug
+      );
+    },
+  },
+};
 </script>
 
 <style>
