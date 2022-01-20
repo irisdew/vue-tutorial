bf5f7b851c5074062fac67ee3b346b03d0a470b2
irisdew <ycleardew@gmail.com>
2022-01-21 00:15:24 +0900

    add Meal page and practice vue-router

--- recipe-app/src/router/index.js
+++ recipe-app/src/router/index.js
@@ -1,17 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/meal/:slug",
+    name: "Meal",
+    component: () => import("../views/Meal.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ recipe-app/src/views/Meal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="recipe">
+    <router-link to="/">&lt; Back</router-link>
+    <h1>{{ $route.params.slug }}</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
