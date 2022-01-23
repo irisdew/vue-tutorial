7e4bfd6b9c4cc3c2ccb391e4c6ed7e487ace70d7
irisdew <ycleardew@gmail.com>
2022-01-23 21:45:42 +0900

    feat: update TodoInput

--- doit/todo/src/components/TodoInput.vue
+++ doit/todo/src/components/TodoInput.vue
@@ -1,6 +1,15 @@
 <template>
-  <input type="text" v-model="newTodoItem" />
-  <button @click="addTodo">ADD</button>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Things I have to do..."
+      @keyup.enter="addTodo"
+    />
+    <span class="addContainer" @click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -25,5 +34,33 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 1rem;
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 7px;
+}
+.inputBox input {
+  width: 80%;
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right top, #f0b6d3, #ee94a8);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
