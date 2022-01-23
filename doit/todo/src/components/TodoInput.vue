12649f14becf1f1173d73a046485b5f2815ab19f
irisdew <ycleardew@gmail.com>
2022-01-23 21:31:23 +0900

    feat: update addTodo

--- doit/todo/src/components/TodoInput.vue
+++ doit/todo/src/components/TodoInput.vue
@@ -12,7 +12,14 @@ export default {
   },
   methods: {
     addTodo() {
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      if (this.newTodoItem !== "") {
+        const value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
     },
   },
 };
