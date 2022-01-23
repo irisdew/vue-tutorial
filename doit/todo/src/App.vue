86b99650f4be88361ccb7afece64dbb31fdb3203
irisdew <ycleardew@gmail.com>
2022-01-23 21:28:34 +0900

    style: add css

--- doit/todo/src/App.vue
+++ doit/todo/src/App.vue
@@ -23,4 +23,19 @@ export default {
 </script>
 
 <style>
+body {
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  background-color: #f6f6f8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
