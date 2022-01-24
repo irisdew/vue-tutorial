23435231bd0714167992cfad86d8698a7b3b910c
irisdew <ycleardew@gmail.com>
2022-01-24 23:04:52 +0900

    feat: get todo data from localStorage

--- doit/todo/src/components/TodoList.vue
+++ doit/todo/src/components/TodoList.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem">{{ todoItem }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      todoItems: [],
+    };
+  },
+  created() {
+    // 뷰 인스턴스가 생성되자마자 뷰 데이터에 접근할 수 있도록 created()에서 로컬스토리지 데이터를 뷰 데이터로 옮김
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+};
 </script>
 
 <style>
