2555d906f0904a233bdf3080cb3ee66ef6e73c64
irisdew <ycleardew@gmail.com>
2022-01-25 23:44:11 +0900

    feat: add removeTodo

--- doit/todo/src/components/TodoList.vue
+++ doit/todo/src/components/TodoList.vue
@@ -1,7 +1,17 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems" :key="todoItem">{{ todoItem }}</li>
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span
+          class="removeBtn"
+          type="button"
+          @click="removeTodo(todoItem, index)"
+        >
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
     </ul>
   </section>
 </template>
@@ -20,6 +30,12 @@ export default {
         this.todoItems.push(localStorage.key(i));
       }
     }
+  },
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
   },
 };
 </script>
