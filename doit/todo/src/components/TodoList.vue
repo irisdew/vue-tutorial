437ab4d4c741d0ddea6cc0cb8c7c4c8460f26e4c
irisdew <ycleardew@gmail.com>
2022-01-25 23:53:51 +0900

    style: add TodoList css

--- doit/todo/src/components/TodoList.vue
+++ doit/todo/src/components/TodoList.vue
@@ -40,5 +40,31 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  /* css margin auto는 남아있는 width를 차지하는 것이 기본, 오른쪽 왼쪽 모두에 적용될 경우 남아있는 width를 정확히 반반으로 나눠가짐 */
+  color: #de4343;
+}
 </style>
